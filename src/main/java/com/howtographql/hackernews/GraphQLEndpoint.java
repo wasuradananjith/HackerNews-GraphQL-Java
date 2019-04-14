4c7f28a94e3b4cb583ee0f6075eb7071d64ecc88
Wasura Dananjith <wasuradananjith@gmail.com>
2019-04-14 20:54:42 +0530

    Add source code

---
+++ src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
@@ -0,0 +1,35 @@
+package com.howtographql.hackernews;
+
+import com.coxautodev.graphql.tools.SchemaParser;
+import javax.servlet.annotation.WebServlet;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import graphql.schema.GraphQLSchema;
+import graphql.servlet.SimpleGraphQLServlet;
+
+
+@WebServlet(urlPatterns = "/")
+public class GraphQLEndpoint extends SimpleGraphQLServlet {
+
+    private static final LinkRepository linkRepository;
+
+    static {
+        //Change to `new MongoClient("mongodb://<host>:<port>/hackernews")`
+        //if you don't have Mongo running locally on port 27017
+        MongoDatabase mongo = new MongoClient().getDatabase("hackernews");
+        linkRepository = new LinkRepository(mongo.getCollection("links"));
+    }
+
+    public GraphQLEndpoint() {
+        super(buildSchema());
+    }
+
+    private static GraphQLSchema buildSchema() {
+        return SchemaParser.newParser()
+                .file("schema.graphqls")
+                .resolvers(new Query(linkRepository), new Mutation(linkRepository))
+                .build()
+                .makeExecutableSchema();
+    }
+}
---
+++ src/main/java/com/howtographql/hackernews/Query.java
@@ -0,0 +1,18 @@
+package com.howtographql.hackernews;
+
+import com.coxautodev.graphql.tools.GraphQLRootResolver;
+
+import java.util.List;
+
+public class Query implements GraphQLRootResolver {
+
+    private final LinkRepository linkRepository;
+
+    public Query(LinkRepository linkRepository) {
+        this.linkRepository = linkRepository;
+    }
+
+    public List<Link> allLinks(LinkFilter filter, Number skip, Number first) {
+        return linkRepository.getAllLinks(filter, skip.intValue(), first.intValue());
+    }
+}
